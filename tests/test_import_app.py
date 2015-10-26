f2e1ea9f5838d4536083eeebbe52123708d202db
Mika Tammi <mikatammi@gmail.com>
2015-10-26 20:06:59 +0200

    Implemented the import app test in Python 3 compliant way
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- tests/test_import_app.py
+++ tests/test_import_app.py
@@ -1,4 +1,7 @@
 import os
 
 def test_import_app():
-    execfile('bin/sportstrackercalendarizer-server')
+    app_filename = 'bin/sportstrackercalendarizer-server'
+    with open(app_filename) as f:
+        code = compile(f.read(), app_filename, 'exec')
+        exec(code)
